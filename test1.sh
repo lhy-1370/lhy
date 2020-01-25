593ce2e0896d8e0b4a66e1ea6a0c1ec8dc71d50f
lhy-1370 <lhy1370@gmail.com>
2020-01-25 12:48:48 +0900

    awk test 1

--- test1.sh
+++ test1.sh
@@ -13,8 +13,18 @@ echo "Test Shell Start"
 
 awk '
 BEGIN{print "START"; ok=0}
-/SVGNAME/ {print; ok++}
-END{print ok}
+{ if ( $0 ~ /SVGNAME/ && index($1,"#")==0 )
+	{ok++; SVCNAME[ok]=$1; SVGNAME[ok]=$2;}
+  else if ( $0 ~ /OBJECTNAME/ && index($1,"#")==0 ) 
+	  { OBJECTNAME[ok]=$1 }
+}
+END{
+for (i=1; i<ok; i++)
+	if ( OBJECTNAME[i] != "" )
+		print SVCNAME[i] " | " SVGNAME[i] " | " OBJECTNAME[i]
+	else
+		print SVCNAME[i] " | " SVGNAME[i] " | OBJECTNAME=" SVCNAME[i]
+}
 ' /home/lhy/shellstudy/tpcfg.m
 
 echo "Test Shell End"
